a18f2be3acafcc3e312d03e660a3771eab821c09
Aeiline <nolwenn.leguevel@gmail.com>
2017-03-06 20:45:33 +0000

    init db

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170306204151) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "password"
+    t.integer  "age"
+    t.string   "email"
+    t.string   "homeAddr"
+    t.string   "surname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
