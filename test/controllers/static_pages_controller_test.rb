47aa64c673e1814c5fa0ff3bdc9019b839de18ce
Aeiline <nolwenn.leguevel@gmail.com>
2017-03-06 20:17:29 +0000

    init

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_response :success
+  end
+
+  test "should get help" do
+    get :help
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_response :success
+  end
+
+  test "should get signup" do
+    get :
+
+  test "should get contact" do
+    get :contact
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_response :success
+  end
+    assert_response :success
+  end
+  
+end
