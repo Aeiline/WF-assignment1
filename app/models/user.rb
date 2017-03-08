4751af30a61045fc97b2ac64daed5636c25fee9d
Aeiline <nolwenn.leguevel@gmail.com>
2017-03-08 11:55:53 +0000

    modif mistake?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
-    attr_accessible :password, :username, :userId, :age, :email, :homeAddr, :surname
+    attr_accessor :password, :username, :userId, :age, :email, :homeAddr, :surname
     self.primary_key = :userID
 end
