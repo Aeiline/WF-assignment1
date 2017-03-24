be0dcd819e79a7bff2235e0d4ef0748114e60106
Aeiline <nolwenn.leguevel@gmail.com>
2017-03-24 17:22:13 +0000

    done flash doesnt works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+    
     def show
         @user = User.find(params[:id])
     end
@@ -10,11 +13,12 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            log_in(user)
-          flash[:success] = "Welcome to the Sample App!"
-          redirect_to @user
+            log_in(@user)
+            flash[:success] = "Welcome to the Sample App!"
+            redirect_to @user
         else
-          render 'new'
+            flash[:danger] = "Error in form"
+            render 'new'
         end
     end
 
@@ -28,6 +32,7 @@ class UsersController < ApplicationController
     
     def logged_in_user
         unless logged_in?
+            store_location
             flash[:danger] = "Please log in."
             redirect_to login_url
         end
